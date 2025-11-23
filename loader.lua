b40a6059b61827976adc4f13a50d69bf922d268a
xinsert <yknotkids@gmail.com>
2025-11-23 14:31:39 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
-return loadstring(readfile("vape_local/NewMainScript.lua"))()
+local url = "https://raw.githubusercontent.com/xinsert/vwrewrite-decompiled/main/NewMainScript.lua"
+local success, res = pcall(function()
+    return game:HttpGet(url, true)
+end)
+
+if success then
+    loadstring(res)()
+else
+    warn("[VW Loader] Failed to fetch NewMainScript.lua:", res)
+end
